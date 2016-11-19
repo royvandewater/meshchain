fde2d84bd6d6d6a21f2149d43093fd4b7058928b
sqrtofsaturn <sqrtofsaturn@gmail.com>
2016-11-19 10:06:59 -0700

    Update docs, remove create

--- record/create.go
+++
@@ -1,6 +0,0 @@
-package record
-
-// Create validates, stores, and returns a new record
-func Create() (*Record, error) {
-	return nil, nil
-}
--- record/redis_record.go
+++ record/redis_record.go
@@ -4,18 +4,24 @@ type redisRecord struct {
 	data *Data
 }
 
+// PublicKey return the record's public key. Returns
+// the empty string if the record has no public key.
 func (record *redisRecord) PublicKey() string {
 	return record.data.PublicKey
 }
 
+// Save persists the record in local storage
 func (record *redisRecord) Save() error {
 	return nil
 }
 
+// ToJSON serializes the record and return JSON output
 func (record *redisRecord) ToJSON() (string, error) {
 	return "", nil
 }
 
+// Validate verifies that the record is valid.
+// it returns nil when there are no errors
 func (record *redisRecord) Validate() error {
 	return nil
 }
